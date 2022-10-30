45ea18fd846cf0bdbf06a31b10673f88127fca6a
dtinth on MBP M1 <dtinth@spacet.me>
2022-10-30 22:59:10 +0700

    Sayonara Prisma

--- tracing.js
+++ tracing.js
@@ -3,22 +3,8 @@ const opentelemetry = require('@opentelemetry/sdk-node')
 const {
   getNodeAutoInstrumentations,
 } = require('@opentelemetry/auto-instrumentations-node')
-const { PrismaInstrumentation } = require('@prisma/instrumentation')
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http')
 const { miniTracer } = require('./src/packlets/tracing')
-
-// miniTracer.spanListeners.add({
-//   onStart: (span) => {
-//     console.log(
-//       '>> onStart "%s" (parent=%s, id=%s, trace=%s)',
-//       span.name,
-//       span.parentSpanId,
-//       span.spanContext().spanId,
-//       span.spanContext().traceId,
-//     )
-//   },
-//   onEnd: (span) => {},
-// })
 
 // For troubleshooting, set the log level to DiagLogLevel.DEBUG
 opentelemetry.api.diag.setLogger(
@@ -31,10 +17,7 @@ const baseSpanProcessor = new opentelemetry.tracing.BatchSpanProcessor(exporter)
 
 const sdk = new opentelemetry.NodeSDK({
   spanProcessor: miniTracer.createSpanProcessor(baseSpanProcessor),
-  instrumentations: [
-    getNodeAutoInstrumentations(),
-    new PrismaInstrumentation(),
-  ],
+  instrumentations: [getNodeAutoInstrumentations()],
 })
 
 sdk.start()
