da34495de23b8779beb61a7781adde823a2f4744
dtinth on MBP M1 <dtinth@spacet.me>
2022-11-01 23:22:56 +0700

    Fix problem where a file went missing

---
+++ src/pages/api/scoreboard/[md5]/[playMode]/leaderboard.ts
@@ -0,0 +1,10 @@
+import { createEndpoint } from '@/packlets/next-endpoint'
+import { getLeaderboard } from '@/app/scoreboard'
+
+export default createEndpoint({}).handler(async ({ input, req }) => {
+  const md5 = String(req.query.md5)
+  const playMode = String(req.query.playMode)
+  const max = Math.max(1, Math.min(50, +String(req.query.max) || 50))
+
+  return getLeaderboard(md5, playMode, max)
+})
