005c64b360f5f39bf4b29db72d9a6068a90e1332
Thai Pangsakulyanont <dtinth@spacet.me>
2022-11-27 11:35:18 +0000

    1 file changed, 2 insertions(+), 2 deletions(-)

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
 
-# (cd packages/otel && rm -rf build && mkdir -p build && pnpm pack --pack-destination build)
-# (cd .next/standalone && rm -rf otel && mkdir -p otel && cd otel && echo '{}' > package.json && echo 'packages: []' > pnpm-workspace.yaml && pnpm add --workspace-root ../../../packages/otel/build/*.tgz)
+(cd packages/otel && rm -rf build && mkdir -p build && pnpm pack --pack-destination build)
+(cd .next/standalone && rm -rf otel && mkdir -p otel && cd otel && echo '{}' > package.json && echo 'packages: []' > pnpm-workspace.yaml && pnpm add --workspace-root ../../../packages/otel/build/*.tgz)
 (cd .next/standalone/node_modules && mkdir -p @bemuse && cd @bemuse && rm -rf otel && ln -s ../../otel/node_modules/@bemuse/otel otel)
